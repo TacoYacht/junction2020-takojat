e71e8f7a76f4a1ac46064cf23ae547de95647714
mariporko <mari.porko@aalto.fi>
2020-11-07 00:16:25 +0200

    notification tests

--- app/src/Task.jsx
+++ app/src/Task.jsx
@@ -1,8 +1,16 @@
+import React from "react";
+
 export function Task(task) {
+
+  function notify() {
+    new Notification("hey there");
+  }
+
   return (
     <div className="task-view">
       <p>{"Task name: "}{task.name}</p>
       <p>{"Task description: "}{task.description}</p>
+      <button onClick={notify}>{"Test"}</button>
     </div>
   );
 }
