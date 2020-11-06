4cdd33f45c28301a8dad3ed6ab4ea39716f17449
Henri Merilä <henri.merila@fi.abb.com>
2020-11-07 00:09:04 +0200

    Add get and all functions to DAO.

--- backend/dao.js
+++ backend/dao.js
@@ -1,7 +1,7 @@
 // dao.js
 
 const sqlite3 = require('sqlite3')
-const Promise = require('bluebird') 
+const Promise = require('bluebird')
 
 class AppDAO {
   constructor(dbFilePath) {
@@ -9,11 +9,11 @@ class AppDAO {
       if (err) {
         console.log('Could not connect to database', err)
       } else {
-        console.log('Connected to database')
+        console.log('Connected to ' + dbFilePath)
       }
     })
   }
-    run(sql, params = []) {
+  run(sql, params = []) {
     return new Promise((resolve, reject) => {
       this.db.run(sql, params, function (err) {
         if (err) {
@@ -22,6 +22,34 @@ class AppDAO {
           reject(err)
         } else {
           resolve({ id: this.lastID })
+        }
+      })
+    })
+  }
+
+  get(sql, params = []) {
+    return new Promise((resolve, reject) => {
+      this.db.get(sql, params, (err, result) => {
+        if (err) {
+          console.log('Error running sql: ' + sql)
+          console.log(err)
+          reject(err)
+        } else {
+          resolve(result)
+        }
+      })
+    })
+  }
+
+  all(sql, params = []) {
+    return new Promise((resolve, reject) => {
+      this.db.all(sql, params, (err, rows) => {
+        if (err) {
+          console.log('Error running sql: ' + sql)
+          console.log(err)
+          reject(err)
+        } else {
+          resolve(rows)
         }
       })
     })
