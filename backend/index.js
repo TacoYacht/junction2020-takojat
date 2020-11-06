9c7dc2b53e2bf313249cd81140d59b8aef162bfa
Tommi Askola <tommi.askola@G0766.local>
2020-11-07 00:04:35 +0200

    Add cors

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const AppDAO = require('./dao')
+
+import AppDAO from './dao.js'
+import express from 'express'
+import cors from 'cors'
+
 const app = express();
 const port = 8000;
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
