303957c2232c5b02b45e67ac2166fdc37c8c0783
Tommi Askola <tommi.askola@G0766.local>
2020-11-07 11:23:07 +0200

    Add dummy data to getTasks endpoint

--- backend/getTasks.js
+++ backend/getTasks.js
@@ -1,3 +1,12 @@
-export function getTasksForUser (userId) {
-    return `user${userId}'s tasks`
+export function getTasksForUser (userId, dao) {
+  return [
+    {
+      'name': 'task1',
+      'description': 'This is the first task'
+    },
+    {
+      'name': 'task2',
+      'description': 'This is the second task'
+    }
+]
 }
