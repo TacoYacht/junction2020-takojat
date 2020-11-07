3ee2187ce8bd9ac167fc6636393a01ecbe284f5c
Henri Merilä <henri.merila@fi.abb.com>
2020-11-08 00:13:14 +0200

    Fix getPracticeByType endpoint

--- backend/dummy/dummyPractices.js
+++ backend/dummy/dummyPractices.js
@@ -1,14 +1,14 @@
 export default [
   {
     "title": "Re-energize",
-    "imgpath": "app\src\assets\pixeltrue_yoga_2.svg",
+    "imgpath": "app\\src\\assets\\pixeltrue_yoga_2.svg",
     "description": "Get an energy boost with this short and easy activity break!",
     "duration": 3,
     "type": "activity"
   },
   {
     "title": "Reset your focus",
-    "imgpath": "app\src\assets\pixeltrue_meditation_1.svg",
+    "imgpath": "app\\src\\assets\\pixeltrue_meditation_1.svg",
     "description": "Relax and find your focus with this relaxed mindfullness practice!",
     "duration": 10,
     "type": "mindfullness"
